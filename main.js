5054fb918ce892df956f57c6711fb714c678a49a
FabioRosado <fabioglrosado@gmail.com>
2018-07-31 14:30:43 +0100

    Set up to do area, working button

--- main.js
+++ main.js
@@ -1,75 +1,75 @@
-// Constant variables
-const weatherUrl = "http://api.openweathermap.org/data/2.5/weather?units=metric";
-const weatherApi = "&APPID=" +  "{Your API Key Here}";
-var data = {};
+// // Constant variables
+// const weatherUrl = "http://api.openweathermap.org/data/2.5/weather?units=metric";
+// const weatherApi = "&APPID=" +  "{Your API Key Here}";
+// var data = {};
 
-// Build open weather url
-function buildWeatherUrl(latitude, longitude) {
-    var url = weatherUrl + '&lat=' + latitude + '&lon=' + longitude + weatherApi;
-    return url;
-  }
-
-
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (position) {
-    var lat =  position.coords.latitude;
-    var lon =  position.coords.longitude;
-    getWeather(lat, lon);
-    });
-  } else {
-    console.log("Geolocation is not supported by this browser.");
-  }
+// // Build open weather url
+// function buildWeatherUrl(latitude, longitude) {
+//     var url = weatherUrl + '&lat=' + latitude + '&lon=' + longitude + weatherApi;
+//     return url;
+//   }
 
 
-function getWeather(lat, lon){
-  let url = buildWeatherUrl(lat, lon);
-    axios
-  .get(url)
-  .catch(function (error) {
-    console.log(error);
-  })
-  .then(function (response) {
-    data.city = response.data.name;
-    data.country = response.data.sys.country;
-    data.temp = response.data.main.temp;
-    data.weather = response.data.weather[0].description;
-    data.icon = response.data.weather[0].main;
-    console.log(response);
+// if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(function (position) {
+//     var lat =  position.coords.latitude;
+//     var lon =  position.coords.longitude;
+//     getWeather(lat, lon);
+//     });
+//   } else {
+//     console.log("Geolocation is not supported by this browser.");
+//   }
 
-    weather = document.getElementById('weather-details');
-    weather.innerHTML = getIcon(data.icon) + data.temp + '°C and ' + data.weather + ' in ' + data.city + ", " + data.country;
 
-    updateBg(data.icon);
-  });
-}
+// function getWeather(lat, lon){
+//   let url = buildWeatherUrl(lat, lon);
+//     axios
+//   .get(url)
+//   .catch(function (error) {
+//     console.log(error);
+//   })
+//   .then(function (response) {
+//     data.city = response.data.name;
+//     data.country = response.data.sys.country;
+//     data.temp = response.data.main.temp;
+//     data.weather = response.data.weather[0].description;
+//     data.icon = response.data.weather[0].main;
+//     console.log(response);
 
-function getIcon(icon){
-  var icon = icon.toLowerCase();
-  switch (icon) {
-    case 'thunderstorm':
-      return '<i class="fas fa-bolt fa-lg"></i>';
-    case 'drizzle':
-      return '<i class="fas fa-tint fa-lg"></i>';
-    case 'rain':
-      return '<i class="fas fa-umbrella fa-lg"></i>';
-    case 'snow':
-      return '<i class="fas fa-snowflake fa-lg"></i>';
-    case 'atmosphere':
-      return '<i class="fas fa-braille fa-lg"></i>';
-    case 'clear':
-      return '<i class="fas fa-sun fa-lg"></i>';
-    case 'clouds':
-      return '<i class="fas fa-cloud fa-lg"></i>';
-  }
-}
+//     weather = document.getElementById('weather-details');
+//     weather.innerHTML = getIcon(data.icon) + data.temp + '°C and ' + data.weather + ' in ' + data.city + ", " + data.country;
 
-function updateBg(image){
-  var image = image.toLowerCase();
-  var sheet = document.styleSheets[0];
-  var rulesList = sheet.cssRules || sheet.rules;
-  var htmlRule = rulesList[0];
-  htmlRule.style.backgroundImage = "url('assets/images/" + image + ".jpg')";
-}
+//     updateBg(data.icon);
+//   });
+// }
+
+// function getIcon(icon){
+//   var icon = icon.toLowerCase();
+//   switch (icon) {
+//     case 'thunderstorm':
+//       return '<i class="fas fa-bolt fa-lg"></i>';
+//     case 'drizzle':
+//       return '<i class="fas fa-tint fa-lg"></i>';
+//     case 'rain':
+//       return '<i class="fas fa-umbrella fa-lg"></i>';
+//     case 'snow':
+//       return '<i class="fas fa-snowflake fa-lg"></i>';
+//     case 'atmosphere':
+//       return '<i class="fas fa-braille fa-lg"></i>';
+//     case 'clear':
+//       return '<i class="fas fa-sun fa-lg"></i>';
+//     case 'clouds':
+//       return '<i class="fas fa-cloud fa-lg"></i>';
+//   }
+// }
+
+// function updateBg(image){
+//   var image = image.toLowerCase();
+//   var sheet = document.styleSheets[0];
+//   var rulesList = sheet.cssRules || sheet.rules;
+//   var htmlRule = rulesList[0];
+//   htmlRule.style.backgroundImage = "url('assets/images/" + image + ".jpg')";
+// }
 
 
 let quote = new Vue ({
@@ -86,7 +86,19 @@ let quote = new Vue ({
     hours: 0,
     minutes: 0,
     date: '',
-    time: ''
+    time: '',
+    todo: true,
+    task: '',
+    tasks: [
+      {
+        text: "I need to do this",
+        completed: true
+      },
+      {
+        text: "He needs to do that",
+        completed: false
+      }
+    ]
   },
   methods: {
     getQuote() {
